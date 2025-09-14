ddfd2d9dc567d8dfcb203989abc727977f32b413
agatekarin <agatekarin@gmail.com>
2025-09-14 11:23:50 +0700

    1

--- admin/middleware.ts
+++ admin/middleware.ts
@@ -2,10 +2,9 @@ import NextAuth from 'next-auth';
 import { authConfig } from './src/lib/auth.config';
  
 export default NextAuth(authConfig).auth;
- 
+
 
 // Protect everything except these paths
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\.png$).*)'],
 };
--- admin/next.config.ts
+++ admin/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['bcryptjs', 'pg', 'pg-connection-string', 'pgpass', 'split2'],
+  },
   /* config options here */
 };
 
