9b3a4797ee4786f19e6ad3fff6145a880ad74fd8
agatekarin <agatekarin@gmail.com>
2025-09-14 16:10:17 +0700

    1

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: [
+    "bcryptjs",
+    "pg",
+    "pg-connection-string",
+    "pgpass",
+    "split2",
+  ],
   /* config options here */
 };
 
