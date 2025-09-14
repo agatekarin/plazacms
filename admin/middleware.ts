971ce6ad3e58d2c4e50928cf922a05ad9304cea2
agatekarin <agatekarin@gmail.com>
2025-09-15 06:55:29 +0700

    1

--- admin/middleware.ts
+++ admin/middleware.ts
@@ -2,6 +2,7 @@ export { auth as middleware } from "./src/lib/auth";
 
 // Protect everything except these paths
 export const config = {
+  runtime: "nodejs",
   matcher: [
     "/((?!api/auth|_next|favicon.ico|signin|public|assets).*)",
   ],
